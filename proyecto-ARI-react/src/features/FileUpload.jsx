f5c2d0d6a0768a0db9ad1247b66e83db7f8737f6
000050519 <00050519@uca.edu.sv>
2023-06-03 22:34:47 -0600

    Upload and display file

--- proyecto-ARI-react/src/features/FileUpload.jsx
+++ proyecto-ARI-react/src/features/FileUpload.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import "../App.scss"
 
-export const FileUpload = ({files, setFiles}) => {
+export const FileUpload = ({ files, setFiles, onContentChange }) => {
+  const [file, setFile] = useState(null);
+  const [content, setContent] = useState('');
 
-    const uploadHandler = () => {
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
 
-    } 
+  const handleUpload = async () => {
+    if (file.type !== 'application/json' && file.type !== 'text/plain' && file.type !== 'text/xml') {
+      alert('Invalid file type');
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const fileContent = event.target.result;
+      setContent(fileContent);
+      onContentChange(fileContent); // Pass the content value to the callback function
+    };
+    reader.readAsText(file);
+  };
+
+  const handleButtonClick = () => {
+    handleUpload();
+  };
 
   return (
-    <input type="file"/>
-  )
-}
+    <div>
+      <input type="file" name="file" id="file" onChange={handleFileChange} />
+      <button className="btn btn-secondary"onClick={handleButtonClick}>Upload</button>
+    </div>
+  );
+};
+
+export default FileUpload;
